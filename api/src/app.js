460e8e4f419fbee76984cb94760b3a284391d10b
HigorC <higor_oliveira@outlook.com>
2023-08-31 16:58:57 -0400

    setup cross-env, adding middlewares

---
+++ api/src/app.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from "dotenv"
+import morgan from "morgan"
+import helmet from "helmet"
+import mongoSanitize from "express-mongo-sanitize"
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import fileUpload from "express-fileupload"
+import cors from "cors"
+
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan("dev"))
+}
+
+// middlewares
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(mongoSanitize())
+app.use(cookieParser())
+app.use(compression())
+app.use(fileUpload({ useTempFiles: true }))
+app.use(cors())
+
+app.post('/', (req, res) => {
+    res.send(req.body)
+})
+
+export default app;
