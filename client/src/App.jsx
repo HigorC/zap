833e4695725332636fc91514ab247907a6483e53
HigorC <higor_oliveira@outlook.com>
2023-09-05 08:44:46 -0400

    redux setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
 
-import { ChatIcon } from "./svg"
 function App() {
   return (
     <div className="dark">
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
